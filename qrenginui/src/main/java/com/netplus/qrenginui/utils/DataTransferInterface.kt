c1c13b07f226caec03dcdd89422952359bfccb25
Anyanwu Nicholas(codeBaron) <83093834+codeBaron-dev@users.noreply.github.com>
2025-02-05 08:56:54 +0100

    Delete qrenginui directory

--- qrenginui/src/main/java/com/netplus/qrenginui/utils/DataTransferInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.netplus.qrenginui.utils
-
-import com.netplus.qrengine.backendRemote.model.qr.GenerateQrcodeResponse
-
-interface DataTransferInterface {
-
-    fun transferData(generateQrcodeResponse: GenerateQrcodeResponse?)
-}
